26b06e4aa466c8ae038f0505bfb882f14d6a93c3
sukrutd <sukrut.dd@gmail.com>
2021-09-01 11:10:24 +0530

    Added security middlewares

--- app.js
+++ app.js
@@ -1,20 +1,54 @@
 const fs = require('fs');
 const path = require('path');
-const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
 const usersRoutes = require('./routes/users-routes');
 const placesRoutes = require('./routes/places-routes');
 const HttpError = require('./models/http-error');
+const mongoSanitize = require('express-mongo-sanitize');
+const rateLimit = require('express-rate-limit');
+const xssClean = require('xss-clean');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const cors = require('cors');
+const hpp = require('hpp');
 
 const app = express();
 
+// Enables Body parser middleware
 app.use(express.json());
 
+// Enables logging middleware during development
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+// Sanitizes data
+app.use(mongoSanitize());
+
+// Sets security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xssClean());
+
+// Sets Rate limiting
+const apiRatelimiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 minutes
+    max: 100
+});
+app.use('/api/', apiRatelimiter);
+
+// Prevents http param pollution
+app.use(hpp());
+
+// Enables CORS
 app.use(cors());
 
+// Enables static serving for uploaded images
 app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 
+// Mounts routers
 app.use('/api/users', usersRoutes);
 app.use('/api/places', placesRoutes);
 
